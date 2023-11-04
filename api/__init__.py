6c61986fec1f3cac1910d37cf6fc39be0e2473d0
abhidharshss <abhidharsh1999@gmail.com>
2023-11-04 10:39:31 +0530

    Organizing the API code into Namespaces

--- api/__init__.py
+++ api/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask_restx import Api
 from .auth.views import auth_namespace
 from .orders.views import orders_namespace
+from .config.config import config_dict
 
-def create_app():
-    app=Flask(__name__)
-    api=Api(app)
-    api.add_namespace(orders_namespace,'/')
-    api.add_namespace(auth_namespace,'/auth')
+# create_app function is responsible for creating and configuring the Flask application.
+
+
+def create_app(config=config_dict['dev']):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    api = Api(app)
+    api.add_namespace(orders_namespace)
+    api.add_namespace(auth_namespace, path='/auth')
     return app
