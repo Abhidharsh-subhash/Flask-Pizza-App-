af33c36885137a00f1632c178047ac519adc5b77
abhidharshss <abhidharsh1999@gmail.com>
2023-11-06 16:43:15 +0530

    database created with sqlalchemy

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,10 @@ from flask_restx import Api
 from .auth.views import auth_namespace
 from .orders.views import orders_namespace
 from .config.config import config_dict
+from .utils import db
+from .models.orders import Order
+from .models.users import User
+from flask_migrate import Migrate
 
 # create_app function is responsible for creating and configuring the Flask application.
 
@@ -10,7 +14,17 @@ from .config.config import config_dict
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
     app.config.from_object(config)
+    db.init_app(app)
+    migrate = Migrate(app, db)
     api = Api(app)
     api.add_namespace(orders_namespace)
     api.add_namespace(auth_namespace, path='/auth')
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return {
+            'db': db,
+            'User': User,
+            'Order': Order
+        }
     return app
